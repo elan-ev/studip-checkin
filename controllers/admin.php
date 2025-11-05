e33663668d82d9b96148a6101b470f6b4a84ffec
Ron Lucke <lucke@elan-ev.de>
2025-11-05 14:55:52 +0100

    add Helpbar text

--- controllers/admin.php
+++ controllers/admin.php
@@ -19,5 +19,13 @@ class AdminController extends PluginController
         if (Navigation::hasItem('/contents')) {
             Navigation::activateItem('/contents/checkin');
         }
+        $helpbar = Helpbar::get();
+        $helpbar->addPlainText('', 
+            _('Das StudipCheckin Plugin') //TODO add more text
+        );
+        $helpbar->addPlainText('',  _('Dieses Plugin wurde vom elan e.V. entwickelt. Es steht unter der GNU Affero General Public License, Version 3. Der vollständige Quellcode ist öffentlich zugänglich im GitHub-Repository.'));
+        $helpbar->addLink('GNU Affero General Public License', 'https://www.gnu.org/licenses/agpl-3.0.de.html', Icon::create('link-extern', Icon::ROLE_INFO_ALT), '_blank');
+        $helpbar->addLink('GitHub-Repository', 'https://github.com/elan-ev/studip-checkin', Icon::create('link-extern', Icon::ROLE_INFO_ALT), '_blank');
+        $helpbar->addLink('elan e.V.', 'https://elan-ev.de', Icon::create('link-extern', Icon::ROLE_INFO_ALT), '_blank');
     }
 }
