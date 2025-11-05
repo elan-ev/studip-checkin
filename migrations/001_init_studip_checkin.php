cf6abb6dcc9fb28d30e19e30d6fee64bebb9729d
Ron Lucke <lucke@elan-ev.de>
2025-11-05 14:28:57 +0100

    add migration

--- migrations/001_init_studip_checkin.php
+++ migrations/001_init_studip_checkin.php
@@ -14,9 +14,59 @@ final class InitStudipCheckin extends Migration
 {
     public function up()
     {
+        $db = DBManager::get();
+
+        $db->exec("CREATE TABLE IF NOT EXISTS `checkin_related_users` (
+            `id`                            INT(11) NOT NULL AUTO_INCREMENT,
+            `form_id`                       INT(11) NOT NULL,
+            `user_id`                       CHAR(32) NOT NULL,
+            `active`                        TINYINT(1) NOT NULL DEFAULT '1',
+            `mkdate`                        INT(11) UNSIGNED NOT NULL,
+            `chdate`                        INT(11) UNSIGNED NOT NULL,
+
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `user_form_unique` (`user_id`, `form_id`),
+            INDEX `status_check_idx` (`user_id`, `active`),
+            INDEX `form_id_idx` (`form_id`)
+            )"
+        );
+
+        $db->exec("CREATE TABLE IF NOT EXISTS `checkin_forms` (
+            `id`                            INT(11) NOT NULL AUTO_INCREMENT,
+            `filter_id`                     char(32)  NOT NULL,
+            `name`                          CHAR(255) NOT NULL,
+            `structure`                     MEDIUMTEXT NOT NULL,
+            `version`                       INT(11) NOT NULL,
+            `mkdate`                        INT(11) UNSIGNED NOT NULL,
+            `chdate`                        INT(11) UNSIGNED NOT NULL,
+
+            PRIMARY KEY (`id`),
+            INDEX `filter_id_idx` (`filter_id`)
+            )"
+        );
+
+        $db->exec("CREATE TABLE IF NOT EXISTS `checkin_form_user_data` (
+            `id`                            INT(11) NOT NULL AUTO_INCREMENT,
+            `form_id`                       INT(11) NOT NULL,
+            `user_id`                       CHAR(32) NOT NULL,
+            `form_data`                     MEDIUMTEXT NOT NULL,
+            `form_version`                  INT(11) NOT NULL,
+            `mkdate`                        INT(11) UNSIGNED NOT NULL,
+            `chdate`                        INT(11) UNSIGNED NOT NULL,
+
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `user_form_unique` (`user_id`, `form_id`),
+            INDEX `form_id_idx` (`form_id`),
+            INDEX `user_id_idx` (`user_id`)
+            )"
+        );
     }
 
     public function down()
     {
+        $db = DBManager::get();
+        $db->exec("DROP TABLE IF EXISTS `checkin_related_users`");
+        $db->exec("DROP TABLE IF EXISTS `checkin_forms`");
+        $db->exec("DROP TABLE IF EXISTS `checkin_form_user_data`");
     }
 }
