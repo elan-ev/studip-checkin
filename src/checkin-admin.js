22c490255d64c0f884c09f92e6a4cbf7786a36ce
Ron Lucke <lucke@elan-ev.de>
2026-03-26 11:49:28 +0100

    fix preferredLanguage

--- src/checkin-admin.js
+++ src/checkin-admin.js
@@ -5,27 +5,28 @@ import { createPinia } from 'pinia';
 import { router } from './router/checkin-admin';
 import { useContextStore } from './store/context';
 
+const el = document.getElementById('studip-checkin-admin-app');
 const preferredLanguage = el?.dataset?.preferredLanguage || null;
-
-const contextStore = useContextStore();
-
-if (preferredLanguage) {
-  contextStore.setPreferredLanguage(preferredLanguage);
-}
 
 const app = createApp(App);
 
 const gettext = createGettext({
     availableLanguages: {
-      en: "English",
-      de: "Deutsch",
+        en: 'English',
+        de: 'Deutsch',
     },
-    defaultLanguage: "de",
+    defaultLanguage: 'de',
 });
 app.use(gettext);
 
 const pinia = createPinia();
 app.use(pinia);
+
+const contextStore = useContextStore();
+
+if (preferredLanguage) {
+    contextStore.setPreferredLanguage(preferredLanguage);
+}
 
 app.use(router);
 
